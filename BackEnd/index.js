464614a5153ec18ae2a54d140d61121eba1227f3
Shubham <shubhamsharma02303@gmail.com>
2024-09-05 01:06:37 +0530

    created Book Route for handle API requests

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import bookRoute from "./route/book.route.js";
+
 const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
+const DbURL = process.env.MongoDbURL;
+
+//MongoDb Connection
+try {
+    mongoose.connect(DbURL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    console.log("Database connection was successful")
+
+} catch (error) {
+    console.log("Error : ",error)
+}
+
+//define Routes
+app.use("/books", bookRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
